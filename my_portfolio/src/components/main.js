426ad97b0fe5bad6977b8ae9761acf83b7aafbd7
mahmood <79115153+mahmoodbashar08@users.noreply.github.com>
2022-07-09 18:11:18 +0300

    .

--- my_portfolio/src/components/main.js
+++ my_portfolio/src/components/main.js
@@ -14,7 +14,7 @@ function Main() {
           <ImA />
         </div>
       </section>
-      <section></section>
+      <section className="sectionTow"></section>
       <section></section>
       <section></section>
     </>
