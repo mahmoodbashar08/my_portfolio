2048cb3515de33b0b4ec864403edc690d205a1ae
mahmood <79115153+mahmoodbashar08@users.noreply.github.com>
2022-07-09 06:13:27 +0300

    .

--- my_portfolio/src/App.js
+++ my_portfolio/src/App.js
@@ -1,8 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  console.log(window.scrollY);
-  return <div className="App"></div>;
+  const [offset, setOffset] = useState(0);
+  const [color, setColor] = useState("red");
+  useEffect(() => {
+    const onScroll = () => setOffset(window.pageYOffset);
+    // clean up code
+    window.removeEventListener("scroll", onScroll);
+    window.addEventListener("scroll", onScroll, { passive: true });
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+  if (offset >= 0 && offset <= 100) {
+    console.log("hello from 0");
+  } else if (offset >= 101 && offset <= 200) {
+    console.log("hello from 101");
+  }
+  return (
+    <div
+      className="App"
+      style={{ height: "4121px", backgroundColor: `${color}` }}
+    ></div>
+  );
 }
 
 export default App;
