2e2a5705425f2af28de9a2eda3241f8cdc3ed48c
mahmood <zozo99999@icloud.com>
2022-09-30 04:36:08 +0300

    .

--- my_portfolio/src/MyRouter.js
+++ my_portfolio/src/MyRouter.js
@@ -1,17 +1,14 @@
 import React from "react";
-import Main from "./components/main";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
 function MyRouter() {
   return (
     <Row>
       <Col md={6} lg={6} sm={0} xs={0} className="d-none d-lg-block d-md-block">
-        1 of 2
+        <p>Left_main </p>
       </Col>
       <Col md={6} lg={6} sm={12} xs={12}>
-        <Main />
+        <p>Right_main</p>
       </Col>
     </Row>
   );
